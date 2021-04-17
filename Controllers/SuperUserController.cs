0fba8b4bf653af70b958c603cc610b251a9a42f9
Kevin Lopez <kevin.lopez@email.com>
2021-04-16 22:54:53 -0400

    Add project files.

---
+++ Controllers/SuperUserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RMS.Models;
+
+namespace RMS.Controllers
+{
+    public class SuperUserController : Controller
+    {
+        // GET: SuperUser
+        /*public ActionResult Index()
+        {
+            var model = new CreateTable();
+            return View(model);
+        }*/
+        RMSEntities dbContext = new RMSEntities();
+        public ActionResult SudoCreatetable()
+        {
+            var model = new CreateTable();
+            return View(model);
+        }
+
+        [HttpPost]
+
+        public ActionResult SudoCreateTable(string tableName, List<string> columnName, List<string> columnType)
+        {
+            
+            Debug.WriteLine(tableName);
+            int count = 0;
+            string parameters = string.Empty;
+            foreach (string elemment in columnName)
+            {
+
+                Debug.WriteLine(columnName[count]);
+                Debug.WriteLine(columnType[count]);
+                parameters = parameters + columnName[count] + " " + columnType[count] + ",";
+                count++;
+            }
+            string sql = $"CREATE TABLE {tableName} ({parameters});";
+            dbContext.Database.ExecuteSqlCommand(sql);
+            return View();
+
+        }
+    }
+}
---
+++ Models/CreateTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RMS.Models
+{
+    public class CreateTable
+    {
+        
+        public string tableName { get; set; }
+        public List<string> columnName { get; set; }
+        public List<string> columnType { get; set; }
+        
+    }
+}
